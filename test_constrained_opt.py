2ac5d7c3ae81eb1b0c468918f7086b9a536893ae
Pooja Rao <poojarao12@gmail.com>
2019-04-29 22:37:07 -0400

    changed the bounds from 0 to 1 and changed the initial guess

--- test_constrained_opt.py
+++ test_constrained_opt.py
@@ -5,23 +5,27 @@ import scipy.optimize as optimize
 import numpy as np
 
 def f(params):
-    print(params)  # <-- you'll see that params is a NumPy array
+    #print(params)  # <-- you'll see that params is a NumPy array
     m, a = params # <-- for readability, assign names to component variables
     return 6.0*m**3 - 4.0*(a+1)**3 - 12.0
 
 def constraint(x):
+    pass
     # replace this with my constraints
-    # PRAO: not sure how to do this part
-    return np.atleast_1d(np.sum(np.abs(x)))
+    # not using this at the moment
+    # but probably a better way to put in the constraints
+    #return np.atleast_1d(np.sum(np.abs(x)))
 
 
-initial_guess = [1, 1]
+initial_guess = [23, 19] 
+# x = [m, a], so x[0] is m and x[1] is a
 result = optimize.minimize(f, initial_guess, 
-              constraints=({"type": "ineq",  
-                            "fun": lambda x: x[0]**3 - ((3.0/2.0)*(x[1]*x[1]+3.0*x[1])+9.0)*x[0]-2.0/3.0*(x[1]**3+3.0*x[1]*x[1]+2.0*x[1])},
-                          {"type": "ineq",
-                            "fun": lambda x: x[1]*(x[1]+1.0)*(x[0]/x[1]*3.0-2.0) - 4.0}),  
-                          bounds=((0,None),(0,None)),options=dict({'maxiter': 10})
+         constraints=({"type": "ineq",  
+                       "fun": lambda x: x[0]**3 - ((3.0/2.0)*(x[1]*x[1]+3.0*x[1])+9.0)*x[0]
+                                       + 2.0/3.0*(x[1]**3+3.0*x[1]*x[1]+2.0*x[1])},
+                      {"type": "ineq",
+                       "fun": lambda x: x[1]*(x[1]+1.0)*(x[0]/x[1]*3.0-2.0) - 4.0}),
+         bounds=((1,None),(1,None)), options=dict({'maxiter': 20})
                           )
 
 if result.success:
