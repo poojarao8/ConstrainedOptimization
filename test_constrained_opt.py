156413cbec0cdad58f42b92032496eb82ce0b201
Pooja Rao <poojarao12@gmail.com>
2019-04-29 21:34:02 -0400

    added the structure for constrained optimization

--- test_constrained_opt.py
+++ test_constrained_opt.py
@@ -2,22 +2,27 @@
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
 
 import scipy.optimize as optimize
+import numpy as np
 
 def f(params):
-    # print(params)  # <-- you'll see that params is a NumPy array
-    a, b, c = params # <-- for readability you may wish to assign names to the component variables
-    return a**2 + b**2 + c**2
+    #print(params)  # <-- you'll see that params is a NumPy array
+    m, a = params # <-- for readability, assign names to component variables
+    return 6.0*m**3 - 4.0*(a+1)**3 - 12.0
 
-initial_guess = [1, 1, 1]
-result = optimize.minimize(f, initial_guess)
+def constraint(x):
+    # replace this with my constraints
+    # PRAO: not sure how to do this part
+    return np.atleast_1d(np.sum(np.abs(x)))
+
+
+initial_guess = [1, 1]
+
+result = optimize.minimize(f, initial_guess, constraints={"fun": constraint, "type": "ineq"})
+
 if result.success:
     fitted_params = result.x
     print(fitted_params)
 else:
     raise ValueError(result.message)
 
-
-# PRAO: my initial goal is to optimize a function that looks like
-# 6m^3 - 4(a+1)^3, where a and b are positive integers
-# There is two other inequalities that are constraints, but not bothered about themat the moment
 
