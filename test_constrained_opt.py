d6911d1fe1603e282fa9ee5b40d939baf2adaad3
Pooja Rao <poojarao12@gmail.com>
2019-04-29 23:39:59 -0400

    cleaned up the script

--- test_constrained_opt.py
+++ test_constrained_opt.py
@@ -1,31 +1,41 @@
-#source:https://stackoverflow.com/questions/13670333/multiple-variables-in-scipys-optimize-minimize
+# References: 
+# https://stackoverflow.com/questions/13670333/multiple-variables-in-scipys-optimize-minimize
+# https://www.youtube.com/watch?time_continue=5&v=cXHvC_FGx24
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html#scipy.optimize.minimize
 
 import scipy.optimize as optimize
 import numpy as np
 
-def f(params):
-    #print(params)  # <-- you'll see that params is a NumPy array
-    m, a = params # <-- for readability, assign names to component variables
+# first define an objective function
+def objective(x):
+    m = x[0] 
+    a = x[1]
     return 6.0*m**3 - 4.0*(a+1)**3 - 12.0
 
-def constraint(x):
-    pass
-    # replace this with my constraints
-    # not using this at the moment
-    # but probably a better way to put in the constraints
-    #return np.atleast_1d(np.sum(np.abs(x)))
+# then define the constraints
+def constraint1(x):
+    m = x[0]
+    a = x[1]
+    return m**3 - ((3.0/2.0)*(a*a+3.0*a)+9.0)*m + 2.0/3.0*(a**3+3.0*a*a+2.0*a) 
 
+def constraint2(x):
+    m = x[0]
+    a = x[1]
+    return a*(a+1.0)*(m/a*3.0-2.0) - 4.0
 
-initial_guess = [23, 19] 
-# x = [m, a], so x[0] is m and x[1] is a
-result = optimize.minimize(f, initial_guess, 
-         constraints=({"type": "ineq",  
-                       "fun": lambda x: x[0]**3 - ((3.0/2.0)*(x[1]*x[1]+3.0*x[1])+9.0)*x[0]
-                                       + 2.0/3.0*(x[1]**3+3.0*x[1]*x[1]+2.0*x[1])},
-                      {"type": "ineq",
-                       "fun": lambda x: x[1]*(x[1]+1.0)*(x[0]/x[1]*3.0-2.0) - 4.0}),
-         bounds=((1,None),(1,None)), options=dict({'maxiter': 20})
+# positivity of m and a is enforced via using "bounds", which are bounded below by 1
+# these bounds however do not guarantee that m and a will be integers
+# they take them to be reals >= 1
+
+initial_guess = [23, 20] 
+
+result = optimize.minimize(
+         objective, 
+         initial_guess, 
+         constraints=({"type": "ineq", "fun": constraint1},  
+                      {"type": "ineq", "fun": constraint2}),
+         bounds=((1,None),(1,None)), # these are not integer bounds 
+         options=dict({'maxiter': 20})
                           )
 
 if result.success:
